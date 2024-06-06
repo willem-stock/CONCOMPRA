ad21ed13d7b075636f6a49ebfe93dbad43d5e310
willem-stock <47602544+willem-stock@users.noreply.github.com>
2024-06-06 16:20:53 +0200

    change to v1

--- scripts/transform_sintax.sh
+++
@@ -1,6 +0,0 @@
-sed -i 's/\+.*//g' $1
-sed -i 's/\t\n/\n/g' $1
-sed -i 's/[A-Za-z]://g' $1
-sed -i 's/([^()]*)//g' $1
-sed -i 's/,/\t/g' $1
-sed -i '1 i\OTU_id\tdomain\tphylum\tclass\torder\tfamily\tgenus\tspecies' $1
