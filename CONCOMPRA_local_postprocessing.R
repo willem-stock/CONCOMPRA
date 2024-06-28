cb7daa3ee3a6619ed90e076a1e508f4f1231acce
willem-stock <47602544+willem-stock@users.noreply.github.com>
2024-06-28 17:03:37 +0200

    Update CONCOMPRA_local_postprocessing.R
    to v1

--- CONCOMPRA_local_postprocessing.R
+++ CONCOMPRA_local_postprocessing.R
@@ -1,25 +1,64 @@
-#CONCOMPRA postprocessing script WStock 5march23
+#this script proves an example on how to process the output from CONCOMPRA from a single run and from multiple runs
+#IdTaxa from the Decipher package is used for taxonomic identification; Trained classifiers for this tool can be downloaded here:http://www2.decipher.codes/Downloads.html
+library(DECIPHER); packageVersion("DECIPHER")
+library(phyloseq) packageVersion('phyloseq')
+library(tibble);library(tidyr) 
+setwd("") #set to the directory containing the CONCOMPRA output
+
+load("")# load train classifier Rdata file load(".../SILVA_SSU_r138_2019.RData") 
+
+tax_assign = function(seqs) {
+  ids_CONCOMPRA <- IdTaxa(seqs, trainingSet, strand="both", processors=4, verbose=T, threshold=40) 
+  ranks <- c("domain", "phylum", "class", "order", "family", "genus", "species") # ranks of interest
+  # Convert the output object of class "Taxa" to a matrix analogous to the output from assignTaxonomy
+  taxid_CONCOMPRA <- t(sapply(ids_CONCOMPRA, function(x) {
+    m <- match(ranks, x$rank)
+    taxa <- x$taxon[m]
+    taxa[startsWith(taxa, "unclassified_")] <- NA
+    taxa
+  }))
+  colnames(taxid_CONCOMPRA) <- ranks
+  return (taxid_CONCOMPRA)
+}
+
+#for a single run (having a single consensus sequence file and otu_table, which can contain multiple samples)
+OTU =  read.csv('ill.otu_table.csv', header = TRUE, sep=',')
+OTU =  column_to_rownames(OTU,'X.OTU.ID') %>% as.matrix
+consensus_seq = readDNAStringSet("ill.noglobal_nolocalchim.consensus.sequences.fas")
+consensus_tax = tax_assign(consensus_seq)
+#in case taxonomy was assigned with vsearch:
+concompra_physeq = phyloseq(tax_table(consensus_tax),otu_table(OTU,taxa_are_rows=T), refseq(consensus_seq))
+# add/adjust sample data as required (optional)
+sample_data(concompra_physeq)=data.frame(sample_id=unlist(strsplit(sample_names(concompra_physeq),split='.CONCOMPRA')),
+                                         analysis='CONCOMPRA',row.names = sample_names(concompra_physeq) )
 
 
-install.packages('ape')
-#align with ssu-align & tree construction with FastTree2 #library(ape)
-tree16S<-ape::read.tree('tree.nwk')
-#plot(tree16S)
 
-#taxonomic assignment-vsearch
-vsearch_sintax_out <- read.delim("reads.sintax", header=T,fill=T, row.names = 1)
-vsearch_sintax_out<-as.matrix(vsearch_sintax_out)
+#for multiple runs (having multiple consensus sequence files and otu_table files) ####
+#this script assumes that all otu tables are named SAMPLEID.otu_table.csv and that the matching consensus sequence file is called SAMPLEID.noglobal_nolocalchim.consensus.sequences.fas 
+#this will load and process all files in the current working directory
+temp = list.files(pattern="*.otu_table.csv") #this should find all otu tables (adjust if you have named the otu tables differently)
+for (i in 1:length(temp)) { #merge all CONCOMPRA output files with tax info
+  id = unlist(strsplit(temp[i],split='.otu_table.'))[1]
+  assign(paste(id,'_OTU',sep=""), read.csv(temp[i], header = TRUE, sep=','))
+  assign(paste(id,'_OTU',sep=""), column_to_rownames(get(paste(id,'_OTU',sep="")),'X.OTU.ID') %>% as.matrix)
+  assign(paste(id,'_seq',sep=""),readDNAStringSet(paste(id,".noglobal_nolocalchim.consensus.sequences.fas", sep="")))
+  assign(paste(id,'_seq',sep=""),tax_assign(get(paste(id,'_seq',sep=""))))
+  assign(paste("CONCOMPRA_",id,sep=""),phyloseq(tax_table(get(paste(id,'_seq',sep=""))),otu_table(get(paste(id,'_OTU',sep="")),taxa_are_rows=T)))
+}
 
-# -> to phyloseq -> merge
-#https://joey711.github.io/phyloseq/install.html
-library(phyloseq) #version?
+phyloseqlist_concompra=ls(pattern="CONCOMPRA_")
+physeq_list <- lapply(phyloseqlist_concompra, get)
 
-OTU = otu_table(seqs_nochim_long[3:ncol(seqs_nochim_long)], taxa_are_rows = TRUE)
-TAX=tax_table(vsearch_sintax_out)
-sample_data=read.table("sample_meta_Loes16S4jul22.txt",sep='\t', header=T, row.names = 1)
-which(!rownames(sample_data)%in%colnames(seqs_nochim_long)[3:ncol(seqs_nochim_long)])
+for (i in seq_along(physeq_list)) { #this creates unique taxon names
+  taxa_names(physeq_list[[i]])<-paste(taxa_names(physeq_list[[i]]),'uni',as.character(i))
+}
+combined_concompra_physeq <- do.call(merge_phyloseq, physeq_list) #create a single phyloseq object with all the data
+# add/adjust sample data as required (optional)
+sample_data(combined_concompra)=data.frame(sample_id=unlist(strsplit(sample_names(combined_concompra_physeq),split='.CONCOMPRA')),analysis='CONCOMPRA',row.names = sample_names(combined_concompra_physeq) )
 
-phylo=phyloseq(OTU,TAX, sample_data(sample_data),tree16S) 
+#all data are now in a single phyloseq object
 
-phylo_merge=tax_glom(phylo,taxrank='genus') #merge OTUs based on taxonomic identification
-phylo_merge=tip_glom(phylo,h = 0.05) #merge OTUs based on phylogenetic tree 
+#optional processing steps ####
+combined_concompra_physeq_genus = tax_glom(combined_concompra_physeq,'genus') #merges OTUs that have the same taxonomy at a certain taxonomic rank
+combined_concompra_physeq_genus = transform_sample_counts(combined_concompra_physeq_genus, function(x) 100 * x/sum(x)) #transform the reads counts to % [0-100] relative abundances
